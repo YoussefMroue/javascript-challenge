0b42d030048c3cd73f3a5835823593274771d0ca
YoussefMroue <41927454+YoussefMroue@users.noreply.github.com>
2020-01-28 22:22:53 -0600

    Final Touches

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,34 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select('tbody');
 
-// YOUR CODE HERE!
+tableData.forEach(entry =>{
+    var row = tbody.append('tr');
+    Object.entries(entry).forEach(([key,value])=>{
+        var cell = row.append('td');
+        cell.text(value);
+    });
+    
+
+});
+
+var button = d3.select("#filter-btn");
+
+button.on("click",function(){
+
+    var inputElement = d3.select("#datetime");
+    var inputvalue = inputElement.property("value");
+    console.log("click");
+
+    var filter = tableData.filter(entry => entry.datetime === inputvalue);
+
+    tbody.html("");
+
+    filter.forEach(entry => {
+        var row = tbody.append("tr");
+        Object.entries(entry).forEach(([key,value]) =>{
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
